6ada340c43a5ef711e12fb227dfd01bcb8b68ef1
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2025-03-03 23:58:29 +0000

    #25 fix tf state issue

--- mtc-terransible/backend.tf
+++ mtc-terransible/backend.tf
@@ -1,5 +1,4 @@
 terraform {
-required_version = "~> 1.9.5"
   backend "s3" {
     bucket         = "terransible-tf-state-300"  # Replace with your actual S3 bucket name
     key            = "terraform.tfstate"
