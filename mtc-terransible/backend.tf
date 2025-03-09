831cdd377e191ed464706cb61912aff92a29a02d
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2025-03-09 22:02:30 +0000

    #33 ran terraform fmt -recursive & validate

--- mtc-terransible/backend.tf
+++ mtc-terransible/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terransible-tf-state-300"  /* Replace with your actual S3 bucket name */
+    bucket         = "terransible-tf-state-300" /* Replace with your actual S3 bucket name */
     key            = "terraform.tfstate"
     region         = "eu-west-2"
     dynamodb_table = "TerrAnsibleLocks"
